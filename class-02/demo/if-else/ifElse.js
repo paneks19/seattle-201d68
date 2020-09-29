41550fa67a4b8438027ebb27845f57c88f23f3f5
LENA EIVY <applena@gmail.com>
2020-09-29 12:10:38 -0700

    day 1 updated

---
+++ class-02/demo/if-else/ifElse.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var likesTheBoys = prompt('do you like The Boys?');
+
+if(likesTheBoys.toLowerCase() === 'yes' || likesTheBoys.toLowerCase() === 'y'){
+  alert('I like that show too');
+} else {
+  alert('you are missing out');
+}
