f731eebb98a632b674a8e1b135d123ac57bd3484
Alex Angelico <alex.angelico@live.com>
2020-10-15 21:14:30 -0700

    got all functions working

--- class-14/lab/starter-code/js/catalog.js
+++ class-14/lab/starter-code/js/catalog.js
@@ -1,11 +1,12 @@
 /* global Product, Cart */
 
 'use strict';
- 
+
 
 
 // Set up an empty cart for use on this page.
 var cart = new Cart([]);
+var cartItemCounter = 1;
 
 // On screen load, we call this method to put all of the busmall options
 // (the things in the Product.allProducts array) into the drop down list.
@@ -13,19 +14,19 @@ function populateForm() {
 
   //TODO: Add an <option> tag inside the form's select for each product
   var selectElement = document.getElementById('items');
-  for (var i =0; i<allProducts.length; i++) {
+  for (var i = 0; i < Product.allProducts.length; i++) {
 
     var optionElement = document.createElement('option');
-    optionElement.setAttribute('class', 'menuOption');
-    optionElement.setAttribute('value', allProducts[i].name);
+    // optionElement.setAttribute('class', 'menuOption');
+    optionElement.setAttribute('value', Product.allProducts[i].name);
     // optionElement.setAttribute('selected', 'selected');
 
-    optionElement.textContent = allProducts[i].name;
+    optionElement.textContent = Product.allProducts[i].name;
     selectElement.appendChild(optionElement);
 
-    console.log('populate form function', allProducts[i].name);
-    console.log(optionElement.setAttribute('value', allProducts[i].name));
-    console.log('selectElement', selectElement);
+    // console.log('populate form function', allProducts[i].name);
+    // console.log(optionElement.setAttribute('value', allProducts[i].name));
+    // console.log('selectElement', selectElement);
   }
 
 }
@@ -34,11 +35,11 @@ function populateForm() {
 // object, save the whole thing back to local storage and update the screen
 // so that it shows the # of items in the cart and a quick preview of the cart itself.
 function handleSubmit(event) {
-
+  event.preventDefault();
   // TODO: Prevent the page from reloading
-
+  // console.log('ITEAM VALUE', event.target.value);
   // Do all the things ...
-  addSelectedItemToCart();
+  addSelectedItemToCart(event);
   cart.saveToLocalStorage();
   updateCounter();
   updateCartPreview();
@@ -46,17 +47,48 @@ function handleSubmit(event) {
 }
 
 // TODO: Add the selected item and quantity to the cart
-function addSelectedItemToCart() {
+function addSelectedItemToCart(event) {
   // TODO: suss out the item picked from the select list
+  console.log(event.target);
+  console.log('QUANTITY VALUE', event.target.quantity.value);
+  console.log('ITEM VALUE', event.target.items.value);
+  var product = event.target.items.value;
+  var quantity = event.target.quantity.value;
+
   // TODO: get the quantity
   // TODO: using those, add one item to the Cart
+  cart.addItem(product, quantity);
 }
 
 // TODO: Update the cart count in the header nav with the number of items in the Cart
-function updateCounter() {}
+function updateCounter() {
+  // cartItemCounter++;
+
+  var itemCountElement = document.getElementById('itemCount');
+  itemCountElement.textContent = `There are ${cartItemCounter++} items in your cart.`;
+}
 
 // TODO: As you add items into the cart, show them (item & quantity) in the cart preview div
 function updateCartPreview() {
+  var addedCartItem = cart.items[cart.items.length - 1].product;
+  var addedCartItemQuantity = cart.items[cart.items.length - 1].quantity;
+
+  if (document.getElementById('cartContentsList')) {
+    var cartContentsList = document.getElementById('cartContentsList');
+    var cartItemElement = document.createElement('li');
+    cartItemElement.textContent = addedCartItemQuantity + ' x ' + addedCartItem;
+    cartContentsList.appendChild(cartItemElement);
+    // docunment.getElementById('cartContents').appendChild(cartContentsList);
+  } else {
+    var cartContentsElement = document.getElementById('cartContents');
+    var cartContentsList = document.createElement('ul');
+    cartContentsList.setAttribute('id', 'cartContentsList');
+    var cartItemElement = document.createElement('li');
+
+    cartItemElement.textContent = addedCartItemQuantity + ' x ' + addedCartItem;
+    cartContentsList.appendChild(cartItemElement);
+    cartContentsElement.appendChild(cartContentsList);
+  }
   // TODO: Get the item and quantity from the form
   // TODO: Add a new element to the cartContents div with that information
 }
@@ -64,7 +96,7 @@ function updateCartPreview() {
 // function test(){
 //   var storedProductData = localStorage.getItem('storedProductData');
 //   var parsedProductData = JSON.parse(storedProductData);
-  
+
 //   //console.log ('parsed product data', parsedProductData);
 // }
 
